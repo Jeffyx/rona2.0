e1e8d8444899a0e56fb3f3a52dfb0b817c626d42
Jeff <jeff8er@aol.com>
2020-06-04 15:36:02 -0400

    / path

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('covidgraphics.urls')),
     path('covidgraphics/', include('covidgraphics.urls')),
     path('admin/', admin.site.urls),
 ]
