8054924f1e2af64a63ca27646dcab249792bfc23
Jeff <jeff8er@aol.com>
2020-06-04 15:52:05 -0400

    broken app

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+from dj_static import Cling
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
+
+#application = get_wsgi_application()
